199734bee8e6a6b9d5a8993c4dab65dd551aa415
sergio durval <sergiodurval2@gmail.com>
2021-04-20 00:34:52 -0300

    classes que representam os dados a serem extraidos

---
+++ src/SoccerBet.Extractor/Models/Match.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SoccerBet.Extractor.Models
+{
+    public class Match
+    {
+        public int Number { get; set; }
+        public string MatchDate { get; set; }
+        public Teams[] Teams { get; set; }
+    }
+}
---
+++ src/SoccerBet.Extractor/Models/Teams.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SoccerBet.Extractor.Models
+{
+    public class Teams
+    {
+        public string Name { get; set; }
+        public string ShieldImage { get; set; }
+        public string ImagemUpload { get; set; }
+    }
+}
