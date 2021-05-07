aadc6c5b2dbca2f2b9c692c01957e49c0f6c352d
sergio durval <sergiodurval2@gmail.com>
2021-05-07 02:25:59 -0300

    iniciando a configuração da DI

--- src/SoccerBet.Extractor/Program.cs
+++ src/SoccerBet.Extractor/Program.cs
@@ -1,15 +1,32 @@
-﻿using System;
-using System.IO;
-using SoccerBet.Extractor.Models;
-using System.Configuration;
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using SoccerBet.Data.Context;
+using Microsoft.EntityFrameworkCore;
+
 namespace SoccerBet.Extractor
 {
     class Program
     {
         static void Main(string[] args)
         {
+            BuildConfig();
             var extraction = new Extraction();
             extraction.ExtractMatch();
+        }
+
+        static void BuildConfig()
+        {
+            var builder = new ConfigurationBuilder()
+               .AddJsonFile($"appsettings.json", true, true);
+
+            var config = builder.Build();
+
+            var services = new ServiceCollection();
+
+            services.AddDbContext<SoccerBetDbContext>(options =>
+            {
+                options.UseSqlServer(config.GetConnectionString("DefaultConnection"));
+            });
         }
 
 
