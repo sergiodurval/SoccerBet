74162a9cbe745f0e732731aa32a20e4fff1ecf8f
sergio durval <sergiodurval2@gmail.com>
2021-05-08 01:18:12 -0300

    Criando a base de dados e primeira migration

---
+++ src/SoccerBet.Data/Migrations/20210508035536_MigracaoInicial.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace SoccerBet.Data.Migrations
+{
+    public partial class MigracaoInicial : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "League",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(nullable: false),
+                    Country = table.Column<string>(type: "varchar(200)", nullable: false),
+                    Name = table.Column<string>(type: "varchar(200)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_League", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Rounds",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(nullable: false),
+                    LeagueId = table.Column<Guid>(nullable: false),
+                    Number = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Rounds", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Rounds_League_LeagueId",
+                        column: x => x.LeagueId,
+                        principalTable: "League",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Restrict);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Matchs",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(nullable: false),
+                    LeagueId = table.Column<Guid>(nullable: false),
+                    RoundId = table.Column<Guid>(nullable: false),
+                    HomeTeam = table.Column<string>(type: "varchar(200)", nullable: false),
+                    AwayTeam = table.Column<string>(type: "varchar(200)", nullable: false),
+                    HomeScoreBoard = table.Column<int>(type: "int", nullable: true),
+                    AwayScoreBoard = table.Column<int>(type: "int", nullable: true),
+                    MatchDate = table.Column<DateTime>(type: "datetime", nullable: false),
+                    CreatedAt = table.Column<DateTime>(type: "datetime", nullable: false),
+                    UpdatedAt = table.Column<DateTime>(type: "datetime", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Matchs", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Matchs_League_LeagueId",
+                        column: x => x.LeagueId,
+                        principalTable: "League",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Restrict);
+                    table.ForeignKey(
+                        name: "FK_Matchs_Rounds_RoundId",
+                        column: x => x.RoundId,
+                        principalTable: "Rounds",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Restrict);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Matchs_LeagueId",
+                table: "Matchs",
+                column: "LeagueId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Matchs_RoundId",
+                table: "Matchs",
+                column: "RoundId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Rounds_LeagueId",
+                table: "Rounds",
+                column: "LeagueId");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Matchs");
+
+            migrationBuilder.DropTable(
+                name: "Rounds");
+
+            migrationBuilder.DropTable(
+                name: "League");
+        }
+    }
+}
