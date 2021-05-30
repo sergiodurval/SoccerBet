bbc40d419ad0bd334be74656f97ff286ea6518b8
sergio durval <sergiodurval2@gmail.com>
2021-05-30 01:30:19 -0300

    adicionando as classes de serviço

---
+++ src/SoccerBet.Business/Interfaces/ILeagueService.cs
@@ -0,0 +1,11 @@
+﻿using SoccerBet.Business.Models;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace SoccerBet.Business.Interfaces
+{
+    public interface ILeagueService
+    {
+        Task<IEnumerable<League>> GetAll();
+    }
+}
---
+++ src/SoccerBet.Business/Services/LeagueService.cs
@@ -0,0 +1,32 @@
+﻿using SoccerBet.Business.Interfaces;
+using SoccerBet.Business.Models;
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace SoccerBet.Business.Services
+{
+    public class LeagueService : ILeagueService
+    {
+        private readonly ILeagueRepository _leagueRepository;
+
+        public LeagueService(ILeagueRepository leagueRepository)
+        {
+            _leagueRepository = leagueRepository;
+        }
+
+        public async Task<IEnumerable<League>> GetAll()
+        {
+            try
+            {
+               var leagues = await _leagueRepository.GetAll();
+               return leagues;
+            }
+            catch (Exception ex)
+            {
+
+                throw ex;
+            }
+        }
+    }
+}
