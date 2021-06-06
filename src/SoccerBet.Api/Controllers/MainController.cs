be46b2ce51c8c62883e8ec8d36b6a2a123d94cf9
sergio durval <sergiodurval2@gmail.com>
2021-06-06 15:10:11 -0300

    implementando a maincontroller

---
+++ src/SoccerBet.Api/Controllers/MainController.cs
@@ -0,0 +1,67 @@
+﻿using System.Linq;
+using Microsoft.AspNetCore.Mvc;
+using SoccerBet.Business.Interfaces;
+using SoccerBet.Business.Notifications;
+using Microsoft.AspNetCore.Mvc.ModelBinding;
+
+namespace SoccerBet.Api.Controllers
+{
+
+    [ApiController]
+    public abstract class MainController : ControllerBase
+    {
+        private readonly INotification _notification;
+
+        protected MainController(INotification notification)
+        {
+            _notification = notification;
+        }
+
+        protected bool OperationIsValid()
+        {
+            return !_notification.HasNotification();
+        }
+
+        protected ActionResult CustomResponse(object result = null)
+        {
+            if (OperationIsValid())
+            {
+                return Ok(new
+                {
+                    sucess = true,
+                    data = result
+                });
+            }
+
+            return BadRequest(new
+            {
+                sucess = false,
+                errors = _notification.GetNotifications().Select(n => n.Message)
+            });
+        }
+
+        protected ActionResult CustomResponse(ModelStateDictionary modelState)
+        {
+            if (!modelState.IsValid)
+                NotifyErrorModelInvalid(modelState);
+
+            return CustomResponse();
+        }
+
+        protected void NotifyErrorModelInvalid(ModelStateDictionary modelState)
+        {
+            var erros = modelState.Values.SelectMany(x => x.Errors);
+
+            foreach (var erro in erros)
+            {
+                var erroMsg = erro.Exception == null ? erro.ErrorMessage : erro.Exception.Message;
+                NotifyError(erroMsg);
+            }
+        }
+
+        protected void NotifyError(string message)
+        {
+            _notification.Handle(new Notification(message));
+        }
+    }
+}
--- src/SoccerBet.Api/Startup.cs
+++ src/SoccerBet.Api/Startup.cs
@@ -1,17 +1,11 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.HttpsPolicy;
-using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
 using SoccerBet.Api.Configuration;
 using SoccerBet.Business.Interfaces;
+using SoccerBet.Business.Notifications;
 using SoccerBet.Business.Services;
 using SoccerBet.Data.Connection;
 using SoccerBet.Data.Interfaces;
@@ -35,6 +29,7 @@ namespace SoccerBet.Api
             services.AddScoped<IConnectionFactory, DefaultSqlConnectionFactory>();
             services.AddScoped<ILeagueRepository, LeagueRepository>();
             services.AddScoped<ILeagueService, LeagueService>();
+            services.AddScoped<INotification, Notifier>();
             services.AddIdentityConfiguration(Configuration);
         }
 
