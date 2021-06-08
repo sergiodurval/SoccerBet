57135ea8be8314336fc5b836220265b2ca6df302
sergio durval <sergiodurval2@gmail.com>
2021-06-07 23:39:25 -0300

    teste accountcontroller

---
+++ tests/SoccerBet.Test/AccountControllerTest.cs
@@ -0,0 +1,68 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Moq;
+using SoccerBet.Api.Controllers;
+using SoccerBet.Business.Interfaces;
+using SoccerBet.Test.Builders;
+using System.Threading.Tasks;
+using Xunit;
+
+
+namespace SoccerBet.Test
+{
+
+    public class AccountControllerTest
+    {
+        
+        //todo:arrumar este teste
+        [Fact(DisplayName = "create user")]
+        public async Task RegisterUserSuccessfully()
+        {
+            //Arrange
+            var registerUserViewModel = UserBuilder.New().Build();
+            var mockNotification = new Mock<INotification>();
+            var userManagerMock = new Mock<UserManager<IdentityUser>>(
+                                Mock.Of<IUserStore<IdentityUser>>(),
+                                null,
+                                null,
+                                null,
+                                null,
+                                null,
+                                null,
+                                null,
+                                null);
+
+            var signInManagerMock = new Mock<SignInManager<IdentityUser>>(
+                                    userManagerMock.Object,
+                                    Mock.Of<IHttpContextAccessor>(),
+                                    Mock.Of<IUserClaimsPrincipalFactory<IdentityUser>>(),
+                                    null,
+                                    null,
+                                    null,
+                                    null);
+
+            var user = new IdentityUser
+            {
+                UserName = registerUserViewModel.UserName,
+                Email = registerUserViewModel.Email,
+                EmailConfirmed = true
+            };
+
+            var mockIdentityResult = new Mock<IdentityResult>();
+            //mockIdentityResult.Setup(r => r.Succeeded == true);
+
+            userManagerMock.Setup(r => r.CreateAsync(user, registerUserViewModel.Password)).Returns(Task.FromResult(mockIdentityResult.Object));
+
+            var controller = new AccountController(mockNotification.Object, signInManagerMock.Object, userManagerMock.Object);
+            
+            //Act
+
+            var actionResult = await controller.Register(registerUserViewModel);
+            userManagerMock.Verify(r => r.CreateAsync(It.Is<IdentityUser>(c => c.UserName == user.UserName && c.Email == user.Email)));
+
+            //Assert
+            Assert.IsType<OkResult>(actionResult);
+        }
+    }
+}
---
+++ tests/SoccerBet.Test/Builders/UserBuilder.cs
@@ -0,0 +1,40 @@
+﻿using Bogus;
+using SoccerBet.Api.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SoccerBet.Test.Builders
+{
+    public class UserBuilder
+    {
+        Faker fake;
+
+        public UserBuilder()
+        {
+            fake = new Faker();
+        }
+
+        public static UserBuilder New()
+        {
+            return new UserBuilder();
+        }
+
+        public RegisterUserViewModel CreateRegisterUser()
+        {
+            var registerUser = new RegisterUserViewModel()
+            {
+                UserName = fake.Person.FirstName,
+                Email = fake.Person.Email,
+                Password = "teste123"
+            };
+
+            return registerUser;
+        }
+
+        public RegisterUserViewModel Build()
+        {
+            return CreateRegisterUser();
+        }
+    }
+}
