ec4b3a4e95658d53eb33c1595d990c79bcdd95b5
sergio durval <sergiodurval2@gmail.com>
2021-06-12 13:53:24 -0300

    implementando o método de adicionar o palpite

--- src/SoccerBet.Data/Repository/BetRepository.cs
+++ src/SoccerBet.Data/Repository/BetRepository.cs
@@ -1,5 +1,7 @@
-﻿using SoccerBet.Business.Interfaces;
+﻿using Dapper;
+using SoccerBet.Business.Interfaces;
 using SoccerBet.Business.Models;
+using SoccerBet.Data.Interfaces;
 using System;
 using System.Threading.Tasks;
 
@@ -7,9 +9,41 @@ namespace SoccerBet.Data.Repository
 {
     public class BetRepository : IBetRepository
     {
-        public Task<Bet> Add(Bet bet)
+        private readonly IConnectionFactory _connection;
+
+        public BetRepository(IConnectionFactory connection)
         {
-            throw new NotImplementedException();
+          _connection = connection;
+        }
+
+        public async Task<Bet> Add(Bet bet)
+        {
+            string sql = @$"insert into [SoccerBet].[dbo].[Bet] values
+                                        (
+                                        Id,
+                                        UserId,
+                                        MatchId,
+                                        HomeScoreBoard,
+                                        AwayScoreBoard,
+                                        CreatedAt)
+                                        values(@Id,@UserId,@MatchId,@HomeScoreBoard,@AwayScoreBoard,CreatedAt)";
+
+            using(var connectionDb = _connection.Connection())
+            {
+                connectionDb.Open();
+
+                await connectionDb.ExecuteAsync(sql, new
+                {
+                    Id = bet.Id,
+                    UserId = bet.UserId,
+                    MatchId = bet.MatchId,
+                    HomeScoreBoard = bet.HomeScoreBoard,
+                    AwayScoreBoard = bet.AwayScoreBoard,
+                    CreatedAt = DateTime.Now
+                });
+
+                return bet;
+            }
         }
     }
 }
